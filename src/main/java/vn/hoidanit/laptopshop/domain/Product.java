42515a27e8524c2e0ee404485c8d3b2b97ad3082
Your Name <minhphp2005@gmail.com>
2024-09-07 10:30:30 +0700

    show view

--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,6 +29,7 @@ public class Product {
     private String image;
 
     @NotNull
+    @Column(columnDefinition = "MEDIUMTEXT")
     @NotEmpty(message = "detailDesc không được để trống")
     private String detailDesc;
 
